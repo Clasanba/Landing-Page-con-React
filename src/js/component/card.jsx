520bd57d82150b13a246323d2053cb2b2a7dda59
Clasanba <clasanba@gmail.com>
2022-08-23 18:01:23 +0000

    Fixed the position of the card and the navbar

--- src/js/component/card.jsx
+++ src/js/component/card.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Card = (props) => {
     return ( <>
-        <div class="card-group card-1 container text-center">
-            <div class="card">
-                <img src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png" class="card-img-top" />
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-                        <p class="card-text">{props.text}</p>
+        <div className="card-group card-1 text-center">
+            <div className="card">
+                <img src="https://edgewoodreit.com/wp-content/uploads/2018/01/500x325.png" className="card-img-top" />
+                    <div className="card-body">
+                        <h5 className="card-title">Card title</h5>
+                        <p className="card-text">{props.text}</p>
                     </div>
-                    <div class="card-footer">
-                        <a href="#" class="btn btn-primary">Find Out More!</a>
+                    <div className="card-footer">
+                        <a href="#" className="btn btn-primary">Find Out More!</a>
                     </div>
             </div>
         </div>
